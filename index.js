b3fd52306bc125827651f79ce619c37473845ff4
Stephanie <lammytonio@gmail.com>
2022-04-04 19:10:51 -0400

    modified code

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogImages.length; i++){
+	dogNames[i].style.borderRadius = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = '#379e35'
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.transform = 'rotate(180deg)'
+}
+
